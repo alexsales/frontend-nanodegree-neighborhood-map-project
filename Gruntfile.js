1da78b663aecaf8b57f52fc3da2ab3e466db5cbc
alexsales <alexsales@sbcglobal.net>
2016-05-19 20:22:57 -0700

    Update summary and GitHub links in README.md; Update Gruntfile to not register tasks related to wiredep; Update bower.json;

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,42 +1,51 @@
 module.exports = function(grunt) {
 
-	grunt.initConfig({
-		wiredep: {
-			target: {
-				src: 'index.html'
-			}
-		},
-		bower_concat: {
-			all: {
-				dest: 'src/js/_bower.js',
-				cssDest: 'src/css/_bower.css'
-			}
-		},
-		uglify: {
-			options: {
-				compress: {
-					drop_console: true
-				},
-				preserveComments: false
-			},
-			my_target: {
-				files: {
-					'src/js/_infobox-icons-main.min.js': ['src/js/infobox.js', 'src/js/map-icons-ie7.js', 'src/js/map-icons.js', 'src/js/main.js']
-				}
-			}
-		},
-		watch: {
-			all: {
-				files: ['./*.html', './src/**/*.css', './src/**/*.js'],
-			}
-		}
-	});
+    grunt.initConfig({
+        wiredep: {
+            options: {
+                exclude: ['jquery']
+            },
+            target: {
+                src: 'index.html'
+            }
+        },
+        bower_concat: {
+            all: {
+                dest: 'src/js/_bower.js',
+                cssDest: 'src/css/_bower.css'
+            }
+        },
+        uglify: {
+            options: {
+                compress: {
+                    drop_console: true
+                },
+                preserveComments: false
+            },
+            my_target: {
+                files: {
+                    'src/js/_infobox-icons-main.min.js': ['src/js/infobox.js', 'src/js/map-icons-ie7.js', 'src/js/map-icons.js', 'src/js/main.js']
+                }
+            }
+        },
+        cssmin: {
+            css_target: {
+                options: {
+                    rebase: false
+                },
+                files: {
+                    'src/css/_offline-styles.min.css': ['src/css/offline-theme-dark.css', 'src/css/offline-theme-dark-indicator.css', 'src/css/offline-language-english.css', 'src/css/offline-language-english-indicator.css', 'src/css/map-icons.css']
+                }
+            }
+        },
+        watch: {
+            all: {
+                files: ['./*.html', './src/**/*.css', './src/**/*.js'],
+            }
+        }
+    });
 
-	// grunt.loadNpmTasks('grunt-wiredep');
-	// grunt.loadNpmTasks('grunt-bower-concat');
-	// all above tasks replace by following line using load-grunt-tasks
+    require('load-grunt-tasks')(grunt);
 
-	require('load-grunt-tasks')(grunt);
-
-	grunt.registerTask('default', ['wiredep', 'bower_concat', 'uglify', 'watch']);
+    grunt.registerTask('default', ['bower_concat', 'uglify', 'cssmin', 'watch']);
 };
